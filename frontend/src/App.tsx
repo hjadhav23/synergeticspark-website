ddb965894beaba562d0a77bfcb89ed6cd7c458a8
hjadhav23 <42672474+hjadhav23@users.noreply.github.com>
2026-08-27 14:57:36 +0530

    Set basename for BrowserRouter in App component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -12,7 +12,7 @@ import "./App.css";
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
